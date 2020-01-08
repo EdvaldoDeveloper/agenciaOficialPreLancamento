f949d375c8946450d9a6a797c3f0b6650ee8ae2e
Edvaldo Silva <edvaldodeveloper@gmail.com>
2020-01-08 03:25:07 -0300

    Ajuste na base de dados

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 var comMySQL = () => {
     console.log('Conexão com bd foi estabelecida');
     return mysql.createConnection({
-        host: ' localhost:3306',
-        user: 'iscodec1_edvaldo-developer',
-        password: 'pr06390721',
-        database: 'iscodec1_agencia'
+        host: 'localhost:3306',
+        user: 'iscodec1_admin',
+        password: 'social4904',
+        database: 'iscodec1_db'
     });
     
 }
--- conteudo/Texto-de-lancamento.md
+++ conteudo/Texto-de-lancamento.md
@@ -2,4 +2,4 @@
 
 ============================<!-- Serviços -->===================================
 
-Estamos chegando com soluções que vai facilita dia a dia, melhorar   processo de atendimento , diminuindo custo de sua operação e potencializando seu resultado .
+Estamos chegando com soluções que vai facilita dia a dia, melhorar  processo de atendimento , diminuindo custo de sua operação e potencializando seu resultado .
